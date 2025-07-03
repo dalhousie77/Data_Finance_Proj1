9a27d2be763d7dbd74974382a4c97c4cfb260c5a
HbDalfin <90664738+dalhousie77@users.noreply.github.com>
2025-07-03 11:12:11 -0300

    Update Data_finance_Proj1.py

--- Data_finance_Proj1.py
+++ Data_finance_Proj1.py
@@ -3,8 +3,7 @@ import pandas as pd
 # If the file is in the same directory:
 df = pd.read_csv("nasdaq_screener.csv")
 
-# If it's somewhere else, use the full path:
-# df = pd.read_csv("C:/Users/YourUsername/Documents/nasdaq_tickers.csv")
+
 
 # Preview the data
 print(df.head(10))
